d674f48f655b2a1a016147bf3b4ccc6a41edb595
tofuujin <1eavinh0pe@snu.ac.kr>
2025-10-22 02:51:22 +0900

    .
    .

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.querySelectorAll('svg g, svg path').forEach(el => {
+  // 마우스 들어올 때
   el.addEventListener('mouseenter', () => {
-    el.style.transform = 'scale(1.1) rotate(15deg)';
+    const bbox = el.getBBox(); // 도형의 경계 박스
+    const cx = bbox.x + bbox.width / 2;
+    const cy = bbox.y + bbox.height / 2;
+    el.style.transformOrigin = `${cx}px ${cy}px`;
+    el.style.transition = 'transform 1s ease';
+    el.style.transform = 'rotate(360deg)';
   });
 
+  // 마우스 나갈 때
   el.addEventListener('mouseleave', () => {
-    el.style.transform = 'scale(1) rotate(0deg)';
+    el.style.transform = 'rotate(0deg)';
   });
 });
