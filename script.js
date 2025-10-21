0ed9fb229e50bda9bf29e7f3ad6a3900e7da033e
tofuujin <1eavinh0pe@snu.ac.kr>
2025-10-22 04:20:40 +0900

    .
    .

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.querySelectorAll('svg g:not([id^="Layer"]), svg path').forEach(el => {
+document.querySelectorAll('#moving-objects g, #moving-objects path').forEach(el => {
   el.addEventListener('mouseenter', () => {
-    const bbox = el.getBBox();
-    const cx = bbox.x + bbox.width / 2;
-    const cy = bbox.y + bbox.height / 2;
-    el.style.transition = 'transform 1s ease';
+    const b = el.getBBox();
+    const cx = b.x + b.width / 2;
+    const cy = b.y + b.height / 2;
     el.style.transformOrigin = `${cx}px ${cy}px`;
     el.style.transformBox = 'fill-box';
     el.style.transform = 'rotate(360deg)';
